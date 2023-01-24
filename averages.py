12e271501401ccb3be04b57674ce040263613f5a
pilipb <philipblecher@hotmail.com>
2023-01-24 14:45:33 +0000

    file

--- averages.py
+++ averages.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
 import sys
+import numpy as np
+
+# calculate mean, median, and mode
+
+def mean(args):
+    return np.mean(args)
+def median(args):
+    return np.median(args)
+def mode(args):
+    return np.mode(args)
+
 
 # take command line inputs and calculate the mean, median, and mode depending on flags
 
@@ -14,10 +25,40 @@ if "--median" in all_args:
     flags.append("median")
 if "--mode" in all_args:
     flags.append("mode")
+if "--file" in all_args:
+    flags.append("file")
+
 
 # remove flags from list of arguments
 for flag in flags:
     all_args.remove("--" + flag)
+
+# open file if flag is present
+if "file" in flags:
+    
+    try:
+        file = open(all_args[-1], "r")
+        all_args = file.read().split()
+        print(all_args)
+
+        for flag in flags:
+            if flag == "mean":
+                mean = mean(all_args)
+                print(mean)
+            if flag == "median":
+                median = median(all_args)
+                print(median)
+            if flag == "mode":
+                mode = mode(all_args)
+                print(mode)
+            else:
+                print("Invalid flag inputted")
+                exit()
+        
+    except NameError:
+        print("File not found")
+        exit()
+
 
 # convert arguments to floats
 try:
@@ -27,53 +68,39 @@ except ValueError:
     print("Invalid argument(s) inputted")
     exit()
 
-# calculate mean, median, and mode
 
-def mean(args):
-    return sum(args) / len(args)
 
-def median(args):
-    args.sort()
-    if len(args) % 2 == 0:
-        return (args[len(args) // 2] + args[len(args) // 2 - 1]) / 2
-    else:
-        return args[len(args) // 2]
-
-def mode(args):
-    mode = args[0]
-    mode_count = 0
-    for arg in args:
-        arg_count = 0
-        for arg2 in args:
-            if arg == arg2:
-                arg_count += 1
-        if arg_count > mode_count:
-            mode = arg
-            mode_count = arg_count
-    return mode
-
-mean = mean(all_args)
-median = median(all_args)
-mode = mode(all_args)
-
-if len(all_args) == 0:
-    print("No arguments inputted")
-    exit()
-elif len(flags) == 0:
+if len(flags) == 0:
     print("No flags inputted")
+    try:
+        mean = mean(all_args)
+        median = median(all_args)
+        mode = mode(all_args)
+    except ZeroDivisionError:
+        print("No arguments inputted")
+        exit()
     print(mean)
     print(median)
     print(mode)
     exit()
 else:
     for flag in flags:
-        if flag == "mean":
-            print(mean)
-        elif flag == "median":
-            print(median)
-        elif flag == "mode":
-            print(mode)
-    exit()
+        try:
+            if flag == "mean":
+                mean = mean(all_args)
+                print(mean)
+            elif flag == "median":
+                median = median(all_args)
+                print(median)
+            elif flag == "mode":
+                mode = mode(all_args)
+                print(mode)
+        except ZeroDivisionError:
+            print("No arguments inputted")
+            exit()
+
+
+
 
 
 
